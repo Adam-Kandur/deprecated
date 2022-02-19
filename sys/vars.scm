866e5330512ce2a3881035d6cf126f1a504c034f
Adam Kandur <>
2022-02-19 03:16:43 +0300

    add %my-pkgs

--- sys/vars.scm
+++ sys/vars.scm
@@ -3,16 +3,25 @@
   #:export (%my-main-user-groups
             %my-system-packages))
 
+(define %my-pkgs
+  (list emacs
+	emacs-exwm
+	emacs-stuff
+	emacs-magit
+	emacs-vscode-dark-plus
+	emacs-desktop-environment
+	nss-certs))
+
 (define %my-main-user-groups
   '("wheel" "netdev" "audio" "video" "docker" "libvirt" "kvm"))
 
-(define %my-system-packages
-  '("emacs"
-    "emacs-exwm"
-    "emacs-desktop-environment"
-    "emacs-magit"
-    "emacs-stuff"
-    "emacs-emojify"
-    "emacs-editorconfig"
-    "papirus-icon-theme"
-    "nss-certs"))
+;; (define %my-system-packages
+;;   '("emacs"
+;;     "emacs-exwm"
+;;     "emacs-desktop-environment"
+;;     "emacs-magit"
+;;     "emacs-stuff"
+;;     "emacs-emojify"
+;;     "emacs-editorconfig"
+;;     "papirus-icon-theme"
+;;     "nss-certs"))
