7b5739bfe43e8ad498ac26674df5cc0798d7e58c
Adam Kandur <>
2022-02-23 21:54:38 +0300

    fix sys

--- sys/vars.scm
+++ sys/vars.scm
@@ -4,24 +4,13 @@
             %my-system-packages))
 
 (define %my-pkgs
-  (list emacs
-	emacs-exwm
-	emacs-stuff
-	emacs-magit
-	emacs-vscode-dark-plus
-	emacs-desktop-environment
-	nss-certs))
+  (list "emacs"
+	"emacs-exwm"
+	"emacs-stuff"
+	"emacs-magit"
+	"emacs-vscode-dark-plus"
+	"emacs-desktop-environment"
+	"nss-certs"))
 
 (define %my-main-user-groups
   '("wheel" "netdev" "audio" "video" "docker" "libvirt" "kvm"))
-
-;; (define %my-system-packages
-;;   '("emacs"
-;;     "emacs-exwm"
-;;     "emacs-desktop-environment"
-;;     "emacs-magit"
-;;     "emacs-stuff"
-;;     "emacs-emojify"
-;;     "emacs-editorconfig"
-;;     "papirus-icon-theme"
-;;     "nss-certs"))
